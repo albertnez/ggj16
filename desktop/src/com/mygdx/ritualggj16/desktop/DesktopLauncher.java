93925ed4516cd31a0196b99a90bd80ec8160838c
pnkThrepwood <pnkthrepwood@gmail.com>
2016-01-29 20:52:22 +0100

    f1rst push didnt even open the project ololo

---
+++ desktop/src/com/mygdx/ritualggj16/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.ritualggj16.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.ritualggj16.Gaem;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Gaem(), config);
+	}
+}
