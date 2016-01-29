864cfe157fca12a9a16d913b2b6141fef6d88cc1
pnkThrepwood <pnkthrepwood@gmail.com>
2016-01-29 21:38:27 +0100

    le playscreenzor

--- core/src/com/mygdx/ritualggj16/Gaem.java
+++ core/src/com/mygdx/ritualggj16/Gaem.java
@@ -1,27 +1,31 @@
 package com.mygdx.ritualggj16;
 
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Gaem extends ApplicationAdapter {
+public class Gaem extends Game {
 	SpriteBatch batch;
-	Texture img;
-	
+    Engine engine;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+        engine = new PooledEngine(1024, 1024*4, 1024, 1024*4);
+
+        Gdx.graphics.setContinuousRendering(true);
+        setScreen(new PlayScreen(this));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render ()
+    {
+        super.render();
 	}
 }
---
+++ core/src/com/mygdx/ritualggj16/PlayScreen.java
@@ -0,0 +1,66 @@
+package com.mygdx.ritualggj16;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Created by ThrepwooD on 29/01/2016.
+ */
+public class PlayScreen implements Screen {
+
+    Gaem gaem;
+
+    Texture img;
+
+    public PlayScreen(Gaem gaem)
+    {
+        this.gaem = gaem;
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render(float delta)
+    {
+
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        gaem.batch.begin();
+        gaem.batch.draw(img, 0, 0);
+        gaem.batch.end();
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
