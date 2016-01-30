180bac99640db0d355c11c11e3fead6ef9a7e5a1
albertnez <albertmg7@gmail.com>
2016-01-30 04:00:57 +0100

    Moved input to a InputComponent

---
+++ core/src/com/mygdx/ritualggj16/Systems/InputSystem.java
@@ -0,0 +1,81 @@
+package com.mygdx.ritualggj16.Systems;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.math.MathUtils;
+import com.mygdx.ritualggj16.Components.InputComponent;
+import com.mygdx.ritualggj16.Components.PositionComponent;
+import com.mygdx.ritualggj16.Components.VelocityComponent;
+import com.mygdx.ritualggj16.Constants;
+import com.mygdx.ritualggj16.Factorys.BulletFactory;
+import com.mygdx.ritualggj16.Mappers;
+import com.mygdx.ritualggj16.XBox360Pad;
+
+/**
+ * Created by anon on 1/30/16.
+ */
+public class InputSystem extends IteratingSystem
+{
+    Engine engine;
+
+    public InputSystem(Engine engine)
+    {
+        super(Family.all(InputComponent.class).get());
+        this.engine = engine;
+    }
+
+    public void processEntity(Entity entity, float deltaTime) {
+        Controller controller = Mappers.input.get(entity).controller;
+        PositionComponent pos = Mappers.position.get(entity);
+        VelocityComponent vel = Mappers.velocity.get(entity);
+
+        vel.x = 0.0f;
+        vel.y = 0.0f;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            vel.x -= Constants.RES_X * deltaTime * 10.0f;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            vel.x += Constants.RES_X * deltaTime * 10.0f;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            vel.y -= Constants.RES_Y * deltaTime * 10.0f;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            vel.y += Constants.RES_Y * deltaTime * 10.0f;
+        }
+
+
+        if (controller != null)
+        {
+            if (Math.abs(controller.getAxis(XBox360Pad.AXIS_LEFT_X)) > 0.2)
+                vel.x = controller.getAxis(XBox360Pad.AXIS_LEFT_X) * Constants.RES_X / 8 * 10.0f;
+
+            if (Math.abs(controller.getAxis(XBox360Pad.AXIS_LEFT_Y)) > 0.2)
+                vel.y = -controller.getAxis(XBox360Pad.AXIS_LEFT_Y) * Constants.RES_Y / 8 * 10.0f;
+
+            if (Math.abs(controller.getAxis(XBox360Pad.AXIS_RIGHT_X)) > 0.2 ||
+                    Math.abs(controller.getAxis(XBox360Pad.AXIS_RIGHT_Y)) > 0.2) {
+
+                BulletFactory.shootBullet(
+                        pos.x,
+                        pos.y,
+                        MathUtils.radDeg * MathUtils.atan2(
+                                -controller.getAxis(XBox360Pad.AXIS_RIGHT_Y),
+                                controller.getAxis(XBox360Pad.AXIS_RIGHT_X)
+                        )
+                );
+
+            }
+
+            if (controller.getAxis(XBox360Pad.AXIS_RIGHT_TRIGGER) < -0.75f) {
+                // BulletFactory.shootBullet(pos.x, pos.y, 0);
+            }
+        }
+    }
+}
