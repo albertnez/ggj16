aa5592c826d7af621b1481730376b8fddf53b84b
pnkThrepwood <pnkthrepwood@gmail.com>
2016-01-30 11:26:51 +0100

    losetas sprites

--- core/src/com/mygdx/ritualggj16/Factorys/ItemFactory.java
+++ core/src/com/mygdx/ritualggj16/Factorys/ItemFactory.java
@@ -1,11 +1,13 @@
 package com.mygdx.ritualggj16.Factorys;
 
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.mygdx.ritualggj16.Components.AnimationComponent;
 import com.mygdx.ritualggj16.Components.CollisionComponent;
 import com.mygdx.ritualggj16.Components.PositionComponent;
 import com.mygdx.ritualggj16.Components.RenderComponent;
 import com.mygdx.ritualggj16.Components.TypeComponent;
+import com.mygdx.ritualggj16.Components.VelocityComponent;
 import com.mygdx.ritualggj16.Gaem;
 import com.mygdx.ritualggj16.TextureManager;
 import com.mygdx.ritualggj16.Utils;
@@ -16,6 +18,21 @@ import com.mygdx.ritualggj16.Utils;
 public class ItemFactory
 {
     public static Gaem gaem;
+
+    public static Entity spawnAltar()
+    {
+        Sprite spr_altar = new Sprite(TextureManager.getTexture("altar.png"), 20, 32);
+        Entity altar = gaem.engine.createEntity()
+                .add(new PositionComponent(0, 0))
+                .add(new VelocityComponent(0, 0))
+                .add(new TypeComponent(TypeComponent.EntityType.Altar))
+                .add(new RenderComponent(spr_altar, 2.0f))
+                .add(new CollisionComponent(10, 16));
+        gaem.engine.addEntity(altar);
+
+        return altar;
+    }
+
 
     public static void spawnAltarEnabler(float x, float y)
     {
@@ -32,7 +49,6 @@ public class ItemFactory
         gaem.engine.addEntity(gaem.engine.createEntity()
                         .add(new PositionComponent(x, y))
                         .add(new TypeComponent(TypeComponent.EntityType.Decoration))
-                        .add(new CollisionComponent(10, 10))
                         .add(new RenderComponent(Utils.dumbSprite(16, 32)) )
                         .add(new AnimationComponent(AnimationFactory.candle()))
         );
--- core/src/com/mygdx/ritualggj16/PlayScreen.java
+++ core/src/com/mygdx/ritualggj16/PlayScreen.java
@@ -113,15 +113,7 @@ public class PlayScreen implements Screen {
         }
 
         //Altar
-        Sprite spr_altar = new Sprite(TextureManager.getTexture("altar.png"), 20, 32);
-        altar = gaem.engine.createEntity()
-                .add(new PositionComponent(0, 0))
-                .add(new VelocityComponent(0, 0))
-                .add(new TypeComponent(TypeComponent.EntityType.Altar))
-                .add(new RenderComponent(spr_altar, 2.0f))
-                .add(new CollisionComponent(10, 16));
-        gaem.engine.addEntity(altar);
-
+        altar = ItemFactory.spawnAltar();
         ItemFactory.spawnCandle(50, 50);
 
         // Dumb control points.
