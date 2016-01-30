89f36760e306f29a24f6efb9badfb8682304e12a
albertnez <albertmg7@gmail.com>
2016-01-30 11:40:08 +0100

    Enemies go to the center, and will move as if they were eating the thing

--- core/src/com/mygdx/ritualggj16/Components/EnemyComponent.java
+++ core/src/com/mygdx/ritualggj16/Components/EnemyComponent.java
@@ -1,6 +1,7 @@
 package com.mygdx.ritualggj16.Components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.MathUtils;
 import com.mygdx.ritualggj16.Wikipedia;
 
 import java.util.HashMap;
@@ -18,15 +19,21 @@ public class EnemyComponent implements Component {
     public EnemyType type;
     public float speed;
     public int damage;
+    public float shakePeriod;
+    public float shakeTime;
+    public boolean shakeY;
 
     public EnemyComponent(EnemyType type)
     {
         this.type = type;
+        this.shakeTime = 0.0f;
+        this.shakeY = MathUtils.randomBoolean();
         switch (type)
         {
             case Walker:
                 this.speed = Wikipedia.Speed(type);
                 this.damage = 5;
+                this.shakePeriod = 0.2f;
         }
     }
 }
