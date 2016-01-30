10d4e17486a184fcbd8c9caa167e26274a5b184c
pnkThrepwood <pnkthrepwood@gmail.com>
2016-01-30 02:25:16 +0100

    dumbspr

---
+++ core/src/com/mygdx/ritualggj16/Utils.java
@@ -0,0 +1,15 @@
+package com.mygdx.ritualggj16;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Created by ThrepwooD on 30/01/2016.
+ */
+public class Utils
+{
+    public Sprite dumbSprite(int w, int h)
+    {
+        return new Sprite(null, w, h);
+    }
+
+}
