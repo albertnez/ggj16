3c236998f017b063bc444f0ccd0aa62d750980c4
albertnez <albertmg7@gmail.com>
2016-01-31 04:47:16 +0100

    Added map bounds;

--- core/src/com/mygdx/ritualggj16/Systems/MovementSystem.java
+++ core/src/com/mygdx/ritualggj16/Systems/MovementSystem.java
@@ -7,9 +7,12 @@ import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.math.MathUtils;
 import com.mygdx.ritualggj16.Components.CollisionComponent;
 import com.mygdx.ritualggj16.Components.PositionComponent;
+import com.mygdx.ritualggj16.Components.TypeComponent;
 import com.mygdx.ritualggj16.Components.VelocityComponent;
+import com.mygdx.ritualggj16.Constants;
 import com.mygdx.ritualggj16.Mappers;
 import com.mygdx.ritualggj16.UltraManager;
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 
 /**
  * Created by threpwood on 20/12/2015.
@@ -50,5 +53,14 @@ public class MovementSystem extends IteratingSystem
 
         pos.x += vel.x*deltaTime;
         pos.y += vel.y*deltaTime;
+
+        if (Mappers.type.get(entity).type == TypeComponent.EntityType.Player)
+        {
+            CollisionComponent col = Mappers.collision.get(entity);
+            float width = col.sizeX * 0.5f;
+            float height = col.sizeY * 0.5f;
+            pos.x = MathUtils.clamp(pos.x, -Constants.RES_X*0.5f + width, Constants.RES_X*0.5f - width);
+            pos.y = MathUtils.clamp(pos.y, -Constants.RES_Y*0.5f + height, Constants.RES_Y*0.5f - height);
+        }
     }
 }
